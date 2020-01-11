544dcbfa7a83ec409bdd387395a7ee65903e5de7
franluz <francielle.luz@gmail.com>
2020-01-11 19:21:22 -0300

    correção e implementacao de mapeamento

--- src/main/java/com/example/carros/model/Carro.java
+++ src/main/java/com/example/carros/model/Carro.java
@@ -1,5 +1,6 @@
 package com.example.carros.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,8 +12,21 @@ public class Carro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+    @Column(name = "nome")
 	private String nome;
-	
+	@Column(name = "descricao")
+	private String descricao;
+	@Column(name = "url_foto")
+	private String url_foto;
+	@Column(name = "url_video")
+	private String url_video;
+	@Column(name = "latitude")
+	private String latitude;
+	@Column(name = "longitude")
+	private String longitude;
+	@Column(name = "tipo")
+	private String tipo;
+
 	public Carro(Long id, String nome) {
 		this.id = id;
 		this.nome = nome;
