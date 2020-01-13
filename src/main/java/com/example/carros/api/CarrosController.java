ff5f3f548d2fcd956cb41c7509e23b1724cbf3a5
jcarpim <jecarpim@gmail.com>
2020-01-12 21:55:22 -0300

    ResponseEntity

--- src/main/java/com/example/carros/api/CarrosController.java
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -3,6 +3,8 @@ package com.example.carros.api;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,8 +26,9 @@ public class CarrosController {
 	private CarroService service;
 	
 	@GetMapping()
-	public Iterable<Carro> get() {
-		return service.getCarros();
+	public ResponseEntity<Iterable<Carro>> get() {
+		return ResponseEntity.ok(service.getCarros());
+		//return  new ResponseEntity<>(service.getCarros(), HttpStatus.OK);
 	}
 	
 	@GetMapping("/{id}")
